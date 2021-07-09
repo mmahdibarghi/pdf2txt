652e6c732aafe35edff2e327c857d62d110e085a
mmahdibarghi <mahdi_b7978@yahoo.com>
2021-07-09 23:37:38 +0430

    add libraries

---
+++ pdf2txt.py
@@ -0,0 +1,13 @@
+# import the following libraries
+# will convert the image to text string
+import pytesseract
+
+# adds image processing capabilities
+from PIL import Image
+
+#split pdf
+from PyPDF2 import PdfFileWriter, PdfFileReader
+#convert pdf to image
+from pdf2image import convert_from_path
+#############################################################
+
